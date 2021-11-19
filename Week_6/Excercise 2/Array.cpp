a86d766d9059703246150c6429860865fbfc7e92
TheUltraBadDuck <92137386+TheUltraBadDuck@users.noreply.github.com>
2021-11-19 11:10:47 +0700

    Update Array.cpp

--- Week_6/Excercise 2/Array.cpp
+++ Week_6/Excercise 2/Array.cpp
@@ -13,12 +13,12 @@ Array::Array(int size) {
         element[i] = 0;
 }
 
-Array::Array(int size, int value) {
+Array::Array(int value[], int size) {
     this->size = size;
     this->element = new int[size];
 
     for (int i = 0; i < size; i++)
-        this->element[i] = value;
+        this->element[i] = value[i];
 }
 
 Array::Array(const Array& arr) {
@@ -53,6 +53,28 @@ int& Array::operator [] (int index) {
 
 Array::operator int* () {
     return element;
+}
+
+istream& operator>>(istream& is, Array& arr) {
+    do {
+        cout << "Enter size of array: ";
+        is >> arr.size;
+    } while (arr.size <= 0 && cout << "Invalid! Please enter again\n");
+
+    // If the array is already defined
+    if (arr.element != NULL)
+        delete[] arr.element;
+
+    arr.element = new int[arr.size];
+
+    cout << "Enter element: ";
+    for (int i = 0; i < arr.size; i++) is >> *(arr.element + i);
+    return is;
+}
+ostream& operator<<(ostream& os, const Array& arr) {
+    cout << "\nArray: ";
+    for (int i = 0; i < arr.size; i++) os << *(arr.element + i) << " ";
+    return os;
 }
 
 // Destructor
