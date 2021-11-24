168cb6bd452a4964502d494505f2abaa5ec74ef5
khangduy017 <78061687+khangduy017@users.noreply.github.com>
2021-11-25 00:45:16 +0700

    Update SATest.cpp

--- Week 8/SATest.cpp
+++ Week 8/SATest.cpp
@@ -1,20 +1,14 @@
 #include "SATest.h"
 
-
 SATest::SATest() {}
 SATest::SATest(SortArray* sa) { SA = sa; }
-
-bool SATest::Check() {
-	if (n <= 0) return false;
-	return true;
-}
+bool SATest::Check() {	return !(n < 1);}
 
 void SATest::Input(istream& is) {
 	cout << "Nhap so phan tu: ";
 	is >> n;
 	for (int i = 0; i < n; i++) {
-		cout << "a[" << i << "] = ";
-		is >> arr[i];
+		cout << "a[" << i << "] = ";is >> arr[i];
 	}
 }
 
