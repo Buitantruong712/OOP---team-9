73b9b6ecc886bc61c6f754ffe2975ca8c56639ab
khangduy017 <78061687+khangduy017@users.noreply.github.com>
2021-11-26 09:58:39 +0700

    Delete Week 8 directory

--- Week 8/InterchangeS.cpp
+++
@@ -1,7 +0,0 @@
-#include "InterchangeS.h"
-
-void InterchangeS::Process(float arr[], int n) {
-	for (int i = 0; i < n - 1; i++)
-		for (int j = i + 1; j < n; j++)
-			if (arr[i] > arr[j]) swap(arr[i], arr[j]);
-}
