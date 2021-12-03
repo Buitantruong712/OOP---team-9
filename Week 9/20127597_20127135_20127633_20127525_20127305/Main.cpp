4f1d365f9194d6404b5ac7740e5f4b3bd6a66da3
Tran-thaoquyen23 <92144715+Tran-thaoquyen23@users.noreply.github.com>
2021-12-03 11:28:10 +0700

    Add files via upload

--- Week 9/20127597_20127135_20127633_20127525_20127305/Main.cpp
+++ Week 9/20127597_20127135_20127633_20127525_20127305/Main.cpp
@@ -1,13 +1,12 @@
 #include "SortAlgorithm.h"
-#include "SortAlgorithm.cpp"
-#include "STUDENT.h"
+
 
 int main() {
 
 	int choice = 1;
 	while (choice != 0) {
 		cout << "\t-------MENU------\n";
-		cout << "\t0. Exit\n\t1. <float>\n\t2. <char> \n\t3. <STUDENT>\n";
+		cout << "\t0. Exit\n\t1. <float>\n\t2. <char> \n\t3. <string>\n\t4. <STUDENT>\n";
 		cin >> choice;
 		switch (choice) {
 		case 0:
@@ -17,7 +16,7 @@ int main() {
 			int n = sizeof(a) / sizeof(a[0]);
 			SortAlgorithm<float>* alg = SortAlgorithm<float>::getObject(SortAlgorithm<float>::InterchangeSort);
 
-			cout << "float a[] = { 1.4F,-5.2F,3.3F,0 } sau khi sap xep : ";
+			cout << "float a[] = { 1.4F ,-5.2F ,3.3F ,0 } sau khi sap xep:\n";
 			alg->Sort(a, n);
 
 			for (int i = 0; i < n; i++) {
@@ -32,21 +31,36 @@ int main() {
 			SortAlgorithm<char>* alg = SortAlgorithm<char>::getObject(SortAlgorithm<char>::InterchangeSort);
 			alg->Sort(a, n);
 
-			cout << "char a[] = { 'E','B','C','D' } sau khi sap xep: ";
+			cout << "char a[] = { 'E','B','C','D' } sau khi sap xep:\n";
 			for (int i = 0; i < n;i++) {
-				cout << a[i] << "\t";
+				cout << a[i] << " ";
 			}
 			cout << endl;
 			break;
 		}
 		case 3: {
-			STUDENT a[] = { new STUDENT("abc",8.5) , new STUDENT("def", 5.5) };
+			string a[] = { "def","ghi","abc" };
+			int n = sizeof(a) / sizeof(a[0]);
+			SortAlgorithm<string>* alg = SortAlgorithm<string>::getObject(SortAlgorithm<string>::InterchangeSort);
+			alg->Sort(a, n);
+
+			cout << "string a[] = {" << (char)34 << "def" << (char)34<<","<<char(34) << "ghi" << (char)34 << "," << char(34) << "abc" << (char)34 << "} sau khi sap xep: \n";
+
+			for (int i = 0; i < n;i++) {
+				cout << a[i] << " ";
+			}
+			cout << endl;
+			break;
+		}
+		case 4:
+		{
+			STUDENT a[] = { new STUDENT("abc",8.5) , new STUDENT("def", 5.5), new STUDENT("abc",7.5) };
 			int n = sizeof(a) / sizeof(a[0]);
 
 			SortAlgorithm<STUDENT>* alg = SortAlgorithm<STUDENT>::getObject(SortAlgorithm<STUDENT>::InterchangeSort);
 			alg->Sort(a, n);
 
-			cout << "STUDENT a[] = { new STUDENT("<<(char)34<<"abc"<<(char)34<<",8.5) , new STUDENT("<<(char)34<<"def"<<(char)34<<", 5.5) } sau khi sap xep theo diem tang dan:\n";
+			cout << "STUDENT a[] = { new STUDENT(" << (char)34 << "abc" << (char)34 << ",8.5) , new STUDENT(" << (char)34 << "def" << (char)34 << ", 5.5), new STUDENT("<<char(34)<<"abc"<<char(34)<<",7.5) } sau khi sap xep: \n";
 			for (int i = 0; i < n;i++) {
 				cout << a[i] << endl;
 			}
--- Week 9/20127597_20127135_20127633_20127525_20127305/SortAlgorithm.h
+++ Week 9/20127597_20127135_20127633_20127525_20127305/SortAlgorithm.h
@@ -2,6 +2,7 @@
 #define _SORTALGORITHM_H_
 
 #include <iostream>
+#include <string>
 using namespace std;
 
 template <class T>
@@ -18,5 +19,149 @@ public:
 	void Sort(T[], int);
 };
 
+
+template <class T>
+SortAlgorithm<T>* SortAlgorithm<T>::_algorithm = NULL;
+
+template <class T>
+SortAlgorithm<T>::SortAlgorithm() {
+	currentAlgorithm = InsertionSort;
+}
+
+
+template <class T>
+SortAlgorithm<T>* SortAlgorithm<T>::getObject(void (*pAlg)(T[], int)) {
+	if (_algorithm == NULL) {
+		_algorithm = new SortAlgorithm();
+	}
+	if (pAlg != NULL) {
+		_algorithm->currentAlgorithm = pAlg;
+	}
+	return _algorithm;
+}
+
+
+template <class T>
+void SortAlgorithm<T>::SelectionSort(T a[], int n) {
+	int min;
+	for (int i = 0; i < n - 1;i++) {
+		min = i;
+		for (int j = i + 1;j < n;j++) {
+			if (a[j] < a[min]) {
+				min = j;
+			}
+		}
+		if (min != i) {
+			T temp = a[min];
+			a[min] = a[i];
+			a[i] = temp;
+		}
+	}
+}
+
+template <class T>
+void SortAlgorithm<T>::InsertionSort(T a[], int n) {
+	int pos;
+	T x;
+	for (int i = 0; i < n;i++) {
+		x = a[i];
+		for (pos = i;(pos > 0 && a[pos - 1] > x);pos--) {
+			a[pos] = a[pos - 1];
+		}
+		a[pos] = x;
+	}
+}
+
+
+template <class T>
+void SortAlgorithm<T>::InterchangeSort(T a[], int n) {
+	for (int i = 0; i < n - 1;i++) {
+		for (int j = i + 1;j < n;j++) {
+			if (a[j] < a[i]) {
+				T temp = a[i];
+				a[i] = a[j];
+				a[j] = temp;
+			}
+		}
+	}
+}
+
+
+template <class T>
+void SortAlgorithm<T>::Sort(T a[], int n) {
+	if (currentAlgorithm != NULL) {
+		currentAlgorithm(a, n);
+	}
+}
+
+
+
+
+class STUDENT {
+	string name;
+	double grade;
+public:
+	STUDENT();
+	STUDENT(string, double);
+	STUDENT(const STUDENT&);
+	STUDENT(STUDENT*);
+	double get_Grade();
+	bool operator<(const STUDENT&);
+	bool operator>(const STUDENT&);
+	friend istream& operator>>(istream&, STUDENT&);
+	friend ostream& operator<<(ostream&, STUDENT);
+
+	~STUDENT();
+};
+
+STUDENT::STUDENT() {
+	name = " ";
+	grade = 0;
+}
+STUDENT::STUDENT(string _name, double _grade) {
+	if (_grade < 0)
+		_grade = 0;
+	name = _name;
+	grade = _grade;
+}
+
+STUDENT::STUDENT(const STUDENT& s) {
+	name = s.name;
+	grade = s.grade;
+}
+
+
+STUDENT::STUDENT(STUDENT* s) {
+	name = s->name;
+	grade = s->grade;
+}
+
+bool STUDENT::operator<(const STUDENT& s) {
+	return this->grade < s.grade;
+}
+
+bool STUDENT::operator>(const STUDENT& s) {
+	return this->grade > s.grade;;
+}
+
+istream& operator>>(istream& istr, STUDENT& s) {
+	getline(istr, s.name);
+	do {
+		istr >> s.grade;
+	} while (s.grade < 0 && cout << "Invalid value. Please enter again!\n");
+	return istr;
+}
+
+double STUDENT::get_Grade() {
+	return grade;
+}
+
+ostream& operator<<(ostream& ostr, STUDENT s) {
+	ostr << s.name << " " << s.grade;
+	return ostr;
+}
+
+STUDENT::~STUDENT() {
+}
 
 #endif
