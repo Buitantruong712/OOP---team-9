a1e85a541a1cd3c1e594707dcf0b69b1e0ed9565
TheUltraBadDuck <92137386+TheUltraBadDuck@users.noreply.github.com>
2021-12-13 18:04:48 +0700

    Add files via upload

--- The Crossing Game/File_chinh/CGame.h
+++ The Crossing Game/File_chinh/CGame.h
@@ -3,89 +3,58 @@
 
 #include "CPeople.h"
 #include "CBird.h"
-#include "CMonkey.h"
 #include "CCar.h"
 #include "CTruck.h"
-#include "CHelicopter.h"
-#include "CTraffic.h"
 
+enum class Key {
+	UP = 72,
+	DOWN = 80,
+	LEFT = 75,
+	RIGHT = 77,
+	ENTER = 13
+};
 
 class CGAME {
 private:
-	const string MAIN_MENU[4] = { "START GAME ", " LOAD GAME ", " SETTINGS ", "   EXIT   " };
+	const string MAIN_MENU[4] = { "START GAME", "LOAD GAME", "SETTINGS ", "  EXIT   " };
 	const string PAUSE_MENU[3] = { "CONTINUE ", "SAVE GAME ", "MAIN MENU" };
-	CBIRD* ac;			short ac_size = 0;
-	CMONKEY* ak;        short ak_size = 0;
-	CCAR* axh;		    short axh_size = 0;
-	CTRUCK* axt;		short axt_size = 0;
-	CHELICOPTER* att;   short att_size = 0;	// (thay thế sau)
-	CTRAFFIC* light1;	// hàng 1 cho trực thăng
-	CTRAFFIC* light2;	// hàng 4 cho xe tải
-	CTRAFFIC* light3;	// hàng 5 cho xe ô tô
-	CPEOPLE* cn;
+	CPEOPLE cn;
+	CCAR* cars_2;		int cars_2_size;
+	CBIRD* birds;		int birds_size;
+	CBIRD* birds_2;		int birds_2_size;
+	CCAR* cars;			int cars_size;
+	CTRUCK* trucks;		int trucks_size;
+	bool exit_game;
 
 public:
-	bool is_running;
-	bool pressable = true;
 	CGAME();
 	~CGAME();
-	const int max_level = 13;
-	int level = 1;
 
 	// Vẽ
 	void drawTitle();					// Title game	
 	void drawMainMenu(short choice);	// Menu chính
-	void drawSettingsMenu(short choice);// Menu cài đặt
-	void drawSaveMenu();				// Menu save
-	void drawLoadMenu();				// Menu load
 	void drawPauseMenu(short choice);	// Menu dừng game
-
-	void drawLevel(short);
-	void drawTime(short);
 	void drawGame();					// Trong game
-	void drawObjects();
-	void clearPauseMenu();
+
 	// Chạy menu
-	short runMainMenu();
-	short runPauseMenu();
+	void runMenu();					
 
 	// Getter
-	CPEOPLE* getPeople() { return cn; };
-	CANIMAL* getBird() { return ac; };
-	CANIMAL* getMonkey() { return ak; };
-	CVEHICLE* getTruck() { return axt; };
-	CVEHICLE* getCar() { return axh; };
-	CVEHICLE* getHelicopter() { return att; };
-	short getBirdSize() { return ac_size; };
-	short getMonkeySize() { return ak_size; };
-	short getCarSize() { return axh_size; };
-	short getTruckSize() { return axt_size; };
-	short getHelicopterSize() { return att_size; };
-
-	bool winning() { return (!endless_mode && level == max_level); };
+	CPEOPLE getPeople();
 
 	// Thao tác game
-	void resetGame(short tt_size, short c_size, short xt_size, short k_size, short xh_size);
-										// Thực hiện thiết lập lại toàn bộ dữ liệu như lúc đầu
-	void exitGame(thread* t);			// Thực hiện thoát Thread 
-	void startGame();					// Thực hiện bắt đầu vào trò chơi
-	void runSettings();					// Thực hiện khi chọn setting
-	void loadGame();					// Thực hiện tải lại trò chơi đã lưu
-	void loadGame(thread* t);			
-	void saveGame(thread* t);			// Thực hiện lưu lại dữ liệu trò chơi
-	void pauseGame(thread* t);			// Tạm dừng Thread
-	void resumeGame(thread* t);			// Quay lai Thread
-	void gameWinner();
-	void gameOver();
+	void resetGame();			// Thực hiện thiết lập lại toàn bộ dữ liệu như lúc đầu
+	void exitGame(HANDLE);		// Thực hiện thoát Thread 
+	void startGame();			// Thực hiện bắt đầu vào trò chơi
+	void loadGame(istream);		// Thực hiện tải lại trò chơi đã lưu
+	void saveGame(istream);		// Thực hiện lưu lại dữ liệu trò chơi
+	void pauseGame(/*HANDLE*/);		// Tạm dừng Thread
+	void resumeGame();			// Quay lai Thread
 
-	void updatePosPeople(char, thread* t);		//Thực hiện điều khiển di chuyển của CPEOPLE
-	void updatePosAnimal();			//Thực hiện cho CTRUCK & CCAR di chuyển
-	void updatePosVehicle();		//Thực hiện cho CDINAUSOR & CBIRD di chuyển
-	void runTraffic();
+	void updatePosPeople(char); //Thực hiện điều khiển di chuyển của CPEOPLE
+	void updatePosAnimal();		//Thực hiện cho CTRUCK & CCAR di chuyển
+	void updatePosVehical();	//Thực hiện cho CDINAUSOR & CBIRD di chuyển
 };
+#endif 
 
-inline void SubThread();
-inline void SoundThread();
-inline void MusicThread();
 
-#endif
